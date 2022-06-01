385a0375ae92b55a3bfa7e58f016549c30029a70
lukaszcichon48 <98329565+lukaszcichon48@users.noreply.github.com>
2022-06-01 17:44:07 +0200

    Update test.js

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ var expect = require('chai').expect;
 
 describe('calculate result', function() {
   it('1 add 1 test', function() {
-    expect(add(1, 1)).to.be.equal(2);
+    expect(add(1, 1)).to.be.equal(3);
   });
 });
