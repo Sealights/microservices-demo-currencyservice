1cda7cb13685c43addee390c5a44c00eb3a28aa7
adibsealights <98812410+adibsealights@users.noreply.github.com>
2022-07-07 04:52:51 +0300

    Update server.js

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ function _carry (amount) {
  * Lists the supported currencies
  */
 function getSupportedCurrencies (call, callback) {
-  logger.info('Getting supported currencies... 04:46 ');
+  logger.info('Getting supported currencies... 04:52 ');
   _getCurrencyData((data) => {
     callback(null, {currency_codes: Object.keys(data)});
   });
