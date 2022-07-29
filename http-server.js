2a5e579ce16716f796aacc0002d2b4dd0ec3a8ea
andrewzenkov <andrew.zenkov.me@gmail.com>
2022-07-29 14:26:12 +0200

    Added http server

---
+++ http-server.js
@@ -0,0 +1,52 @@
+/*
+/*
+ * Copyright 2018 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const { convert, getSupportedCurrencies } = require('./core')
+
+
+const app = express()
+const port = 7001
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/get-supported-currencies', (req, res) => {
+	getSupportedCurrencies(req.body, (err, result) => {
+		if(err) {
+			return res.status(400).send(err)
+		}
+
+		return res.send(result)
+	})
+})
+
+app.post('/convert', (req, res) => {
+	convert(req.body, (err, result) => {
+		if(err) {
+			return res.status(400).send(err)
+		}
+
+		return res.send(result)
+	})
+})
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`)
+})
