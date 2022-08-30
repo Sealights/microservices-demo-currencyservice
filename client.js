1d49a421b72ddd794625f18c3f1d2399a2e3cf1c
Piotr Pernej <98348182+pepem54@users.noreply.github.com>
2022-08-30 12:32:40 +0200

    Update client.js

--- client.js
+++ client.js
@@ -57,7 +57,7 @@ client.getSupportedCurrencies({}, (err, response) => {
 });
 
 client.convert(request, (err, response) => {
-  logger.log(`The life and work of Van Gogh`);
+  logger.log(`The life and work of Van Gogh, part 2`);
   
   if (err) {
     logger.error(`Error in convert: ${err}`);
